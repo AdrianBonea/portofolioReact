422a1ee99442860e4f3b1c2cfcf0929126253ec6
AdrianBonea <boneamirel@gmail.com>
2022-05-31 17:38:32 +0300

    First commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
