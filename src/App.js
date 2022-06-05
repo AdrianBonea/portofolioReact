9ff22b73b0a689a5359ebe92b9e2516dba6b2e63
AdrianBonea <boneamirel@gmail.com>
2022-06-05 20:41:04 +0300

    About me section it is done and styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
 import Contact from "./components/Contact";
-import MainContent from "./components/Main";
+import AboutMe from "./components/AboutMe";
 
 export default function App() {
   return (
     <div className="mx-32 font-poppins">
       <Header />
-      <MainContent />
+      <AboutMe />
       <Contact />
     </div>
   );
